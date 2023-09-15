548475332f4c4fc8658e61e693bb44bdefefd436
Sam-Reinsel <71461485+SamReinsel@users.noreply.github.com>
2023-09-15 15:28:06 -0400

    Create modelAdvisorChecks.m

---
+++ tests/modelAdvisorChecks.m
@@ -0,0 +1,12 @@
+function [results] = modelAdvisorChecks()
+%MODELADVISORCHECKS run Model Advisor checks on project models
+%   
+checkIDs = {'mathworks.maab.jc_0021',...
+'mathworks.iec61508.UnconnectedObjects',...
+'mathworks.maab.jc_0061',...
+'mathworks.maab.db_0142'};
+
+systems = {'demo.slx'};
+results = ModelAdvisor.run(systems,checkIDs);
+end
+
